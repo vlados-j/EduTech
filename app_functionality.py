f146f022d9711b76ea8088ecc75ebee071f96346
Vladyslav Dzhulai <dzhulai.vladyslav@gmail.com>
2023-01-13 16:39:31 -0500

    Refactoring

--- app_functionality.py
+++ app_functionality.py
@@ -1,12 +1,15 @@
 import main
-from models import db, CourseModel, StudentModel
+from models import db, CourseModel, StudentModel, GroupModel
 
 
 def find_all_groups(student_count: int):
     """Find all groups with less or equals student count."""
     with main.app.app_context():
-        all_courses = db.session.execute(db.select(CourseModel))
-        return [course.name for course in all_courses.scalars().all() if len(course.students) <= student_count]
+        all_courses = db.session.execute(db.select(GroupModel.name).
+                                         join(StudentModel, StudentModel.group_id == GroupModel.name).
+                                         group_by(GroupModel.name).
+                                         having(db.func.count(StudentModel.id) <= student_count))
+        return all_courses.scalars().all()
 
 
 def find_students_by_course(course_name: str):
@@ -38,22 +41,20 @@ def add_student_to_course(list_of_courses: list, student_id: int):
     """Add a student to the course (from a list)"""
     with main.app.app_context():
         student = db.session.execute(db.select(StudentModel).where(StudentModel.id == student_id)).scalars().first()
-        all_courses = db.session.execute(db.select(CourseModel.name)).scalars().all()
         if student:
-            for given_course in list_of_courses:
-                if given_course in all_courses:
-                    course = db.session.execute(db.select(CourseModel).
-                                                where(CourseModel.name == given_course)).scalars().first()
-                    student.courses.append(course)
-                    db.session.commit()
+            all_courses = db.session.execute(db.select(CourseModel).
+                                             where(CourseModel.name.in_(list_of_courses))).scalars().all()
+            for given_course in all_courses:
+                student.courses.append(given_course)
+                db.session.commit()
 
 
 def remove_student_from_course(student_id: int, course: str):
     """Remove the student from one of his or her courses"""
     with main.app.app_context():
         student = db.session.execute(db.select(StudentModel).where(StudentModel.id == student_id)).scalars().first()
-        course = db.session.execute(db.select(CourseModel).where(CourseModel.name == course)).scalars().first()
         if student:
+            course = db.session.execute(db.select(CourseModel).where(CourseModel.name == course)).scalars().first()
             if course in student.courses:
                 student.courses.remove(course)
                 db.session.commit()
